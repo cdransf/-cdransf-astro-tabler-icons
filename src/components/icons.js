9c81bc8df7b9aee292d034d0ba0ff31cee228057
Cory Dransfeldt <hi@coryd.dev>
2024-11-21 14:51:12 -0800

    fix: loader

--- src/components/icons.js
+++ src/components/icons.js
@@ -1,10 +1,15 @@
-import ICONS from "../utils/icons.js";
 import Icon from "./Icon.astro";
 
 const components = {};
 
 Object.keys(ICONS).forEach((key) => {
-  if (key !== "HEAD" && key !== "TAIL") components[key] = (props) => <Icon {...props} name={key} />;
+  if (key !== "HEAD" && key !== "TAIL") {
+    components[key] = (props) =>
+      Icon({
+        ...props,
+        name: key,
+      });
+  }
 });
 
 export default components;
